2a5349d7fccb8c042e7a300bd9f957cadf0b7f6e
Arthur <greatdwb@gmail.com>
2018-05-31 19:34:02 +0800

    Update config.sh

--- general/config.sh
+++ general/config.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # YCM need ninja... (the python install script in ninja provide another way to install 
 wget https://github.com/ninja-build/ninja/archive/v1.8.2.tar.gz
 tar xzvf v1.8.2.tar.gz
